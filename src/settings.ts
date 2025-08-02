8e6a22f3745df2018798b04cb3ffbeca09e89748
JhoanDavid28 <pjhoandavid@outlook.com>
2025-08-02 08:56:51 -0500

    feat: Cambios en el contenido

--- src/settings.ts
+++ src/settings.ts
@@ -1,8 +1,8 @@
 export const profile = {
-	fullName: 'Your Name',
-	title: '',
-	institute: '',
-	author_name: '', // Author name to be highlighted in the papers section
+	fullName: 'Jhoan David Pillapa Llerena',
+	title: 'Ing.',
+	institute: 'ESPOCH',
+	author_name: 'Jhoan David Pillapa Llerena', // Author name to be highlighted in the papers section
 	research_areas: [
 		// { title: 'Physics', description: 'Brief description of the research interest', field: 'physics' },
 	],
@@ -11,9 +11,9 @@ export const profile = {
 // Set equal to an empty string to hide the icon that you don't want to display
 export const social = {
 	email: '',
-	linkedin: '',
-	x: 'https://www.x.com/',
-	github: '',
+	linkedin: 'https://www.linkedin.com/in/jhoandavidpll/',
+	x: '',
+	github: 'https://github.com/jhoandavidpll/',
 	gitlab: '',
 	scholar: '',
 	inspire: '',
@@ -21,9 +21,9 @@ export const social = {
 }
 
 export const template = {
-	website_url: 'https://localhost:4321', // Astro needs to know your site’s deployed URL to generate a sitemap. It must start with http:// or https://
+	website_url: 'https://jhoandavidpll.github.io/', // Astro needs to know your site’s deployed URL to generate a sitemap. It must start with http:// or https://
 	menu_left: false,
-	transitions: true,
+	transitions: 	true,
 	lightTheme: 'light', // Select one of the Daisy UI Themes or create your own
 	darkTheme: 'dark', // Select one of the Daisy UI Themes or create your own
 	excerptLength: 200,
@@ -32,7 +32,7 @@ export const template = {
 }
 
 export const seo = {
-	default_title: 'Astro Academia',
-	default_description: 'Astro Academia is a template for academic websites.',
+	default_title: 'Jhoan',
+	default_description: 'Jhoan David Pillapa Llerena | Data Analyst',
 	default_image: '/images/astro-academia.png',
 }
